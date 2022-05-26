b2c9b4b1f5f0046a2610020e3460506895679269
Beth Taiwo <elizabeth@walulel.com>
2022-05-26 23:19:45 +0000

    tailwind setup

---
+++ components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex justify-center items-center">
+    <div class="w-[657px] h-[942px] border border-red-300">
+      WEll well well Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Laboriosam dolores id obcaecati delectus quisquam nostrum eos odit, illum
+      labore beatae voluptatibus et atque libero reiciendis deleniti cupiditate
+      earum sed laudantium!
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage',
+}
+</script>
+
+<style></style>
