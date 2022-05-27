85853e7184840393511e5c8de8b2508505e17ac3
Beth Taiwo <elizabeth@walulel.com>
2022-05-27 20:43:48 +0000

    template setup

---
+++ components/Tasks/PastTasks.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>PastTasks</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
---
+++ components/Tasks/UpcomingTasks.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>UpcomingTask</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
