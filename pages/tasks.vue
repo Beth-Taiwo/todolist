e15cf0c71495d36ca973ee8adc7806a07c404bfe
Beth Taiwo <elizabeth@walulel.com>
2022-05-27 18:57:03 +0000

    navbar setup

---
+++ pages/tasks.vue
@@ -0,0 +1,153 @@
+<template>
+  <div class="py-4 px-9 text-systemBlack">
+    <div class="w-full grid grid-cols-3">
+      <div></div>
+
+      <div class=""></div>
+
+      <div class="flex justify-between">
+        <div class="flex flex-1 justify-center items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            role="img"
+            width="1em"
+            height="1em"
+            preserveAspectRatio="xMidYMid meet"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9m-4.27 13a2 2 0 0 1-3.46 0"
+            />
+          </svg>
+        </div>
+        <div class="avatar flex-1 justify-end items-center space-x-2">
+          <div class="w-11 rounded-full">
+            <img
+              src="https://api.lorem.space/image/face?hash=92310"
+              alt="Profile Image"
+            />
+          </div>
+          <p class="">Janet Janetson</p>
+          <svg
+            class="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M19 9l-7 7-7-7"
+            ></path>
+          </svg>
+        </div>
+      </div>
+    </div>
+    <div class="mt-3 pl-2 pr-4">
+      <p class="font-black text-2xl">Tasks</p>
+      <div class="w-full mb-3">
+        <nav
+          class="w-full whitespace-nowrap overflow-x-auto flex justify-between border-b"
+        >
+          <ul class="flex justify-between w-1/2 mt-5 box-border px-4">
+            <li
+              v-for="nav in navList"
+              :key="nav.component"
+              class="font-semibold border-b-2 pb-0.5 px-1 hover:cursor-pointer hover:border-lightBlue"
+              :class="
+                currentComponent === nav.component
+                  ? 'border-lightBlue text-lightBlue'
+                  : 'border-white'
+              "
+              @click.prevent="changeCurrentComponent(nav.component)"
+            >
+              {{ nav.description }}
+            </li>
+          </ul>
+          <button
+            class="bg-lightBlue text-white px-6 py-1.5 mb-2.5 rounded-[10px] flex items-center justify-center"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              role="img"
+              width="18"
+              height="18"
+              preserveAspectRatio="xMidYMid meet"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-width="2"
+                d="M12 20v-8m0 0V4m0 8h8m-8 0H4"
+              />
+            </svg>
+            <span class="ml-2"> Add task </span>
+          </button>
+        </nav>
+
+        <Component :is="currentComponent" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import PastTasks from '~/components/Tasks/PastTasks.vue'
+import UpcomingTasks from '~/components/Tasks/UpcomingTasks.vue'
+import PresentTasks from '~/components/Tasks/PresentTasks.vue'
+import AllTasks from '~/components/Tasks/AllTasks.vue'
+
+export default {
+  name: 'TasksPage',
+  layout: 'main',
+  components: {
+    PastTasks,
+    UpcomingTasks,
+    PresentTasks,
+    AllTasks,
+  },
+  data() {
+    return {
+      currentComponent: 'AllTasks',
+      navList: [
+        {
+          component: 'AllTasks',
+          description: 'All',
+        },
+        {
+          component: 'PresentTasks',
+          description: 'Today',
+        },
+        {
+          component: 'UpcomingTasks',
+          description: 'Upcoming',
+        },
+
+        {
+          component: 'PastTasks',
+          description: 'Past tasks',
+        },
+      ],
+    }
+  },
+
+  methods: {
+    changeCurrentComponent(component) {
+      this.currentComponent = component
+    },
+  },
+}
+</script>
+
+<style></style>
