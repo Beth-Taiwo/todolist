29f21cbfa015ebb8f40eb2831eda6aaff4ce39cf
Beth Taiwo <elizabeth@walulel.com>
2022-05-27 12:00:14 +0000

    main layout setup

---
+++ layouts/main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex h-screen relative">
+    <LeftMenu />
+    <div class="w-full relative">
+      <nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+// import LeftMenu from '../components/LeftMenu.vue'
+export default {
+  //   components: { LeftMenu },
+}
+</script>
+
+<style></style>
