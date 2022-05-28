d239fbe1f7439f884ad98c867ae5b728cecab286
Beth Taiwo <elizabeth@walulel.com>
2022-05-28 12:17:33 +0000

    WIP: calendar page

---
+++ pages/calendar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="py-4 px-9 text-systemBlack">
+    <div class="mt-14 pl-2 pr-4">
+      <p class="font-black text-2xl">Calendar</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CalendarPage',
+  layout: 'main',
+}
+</script>
+
+<style></style>
